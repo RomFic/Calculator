ca52c2235c82556dd646d6e793439a9aaa3f781c
Romain <70705788+RomFic@users.noreply.github.com>
2025-04-18 09:25:38 -0400

    changed functions-operators parameters and created three variables

--- script.js
+++ script.js
@@ -3,31 +3,37 @@
 const result = document.querySelector('.display');
 const buttons = document.querySelectorAll('.button');
 
+const number1 = 0;
+const number2 = 0;
+const operator = '';
+
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
         const value = e.target.dataset.value;
         result.innerText += value;
 
-        console.log(value);
+
 
 
     })
 })
 
-const add = function (a, b) {
-    return a + b;
+//////////////// Functions math operators /////////////////////////////
+
+const add = function (int1, int2) {
+    return parseInt(int1) + parseInt(int2);
 };
 
-const subtract = function (a, b) {
-    return a - b;
+const subtract = function (int1, int2) {
+    return parseInt(int1) - parseInt(int2);
 };
 
-const multiply = function (a, b) {
-    return a * b;
+const multiply = function (int1, int2) {
+    return parseInt(int1) * parseInt(int2);
 };
 
-const divide = function (a, b) {
-    return a / b;
+const divide = function (int1, int2) {
+    return parseInt(int1) / parseInt(int2);
 };
 
 
@@ -36,7 +42,6 @@ const operate = function (operator, int1, int2) {
 
 
 };
-
 
 
 
