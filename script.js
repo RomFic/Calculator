0d3912c822d60faf65da76d265269d20947be10a
Romain <70705788+RomFic@users.noreply.github.com>
2025-04-19 09:16:28 -0400

    created variables for numbers/operators - created three functions inside for-each

--- script.js
+++ script.js
@@ -3,22 +3,27 @@
 const result = document.querySelector('.display');
 const buttons = document.querySelectorAll('.button');
 
-const number1 = 0;
-const number2 = 0;
-const operator = '';
+let number1 = '';
+let number2 = '';
+let operator = '';
+const operators = '+-*/';
+const numbers = '1234567890.'
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        const value = e.target.dataset.value;
-        result.innerText += value;
+        const buttonValue = e.target.dataset.value;
 
-
-
-
+        if (buttonValue === 'clear') {
+            clearResult();
+        } else if (buttonValue === '=') {
+            operate();
+        } else {
+            appendValue(buttonValue);
+        }
     })
 })
 
-//////////////// Functions math operators /////////////////////////////
+// -- -- -- -- -- -- -- - Functions math operators-- -- -- -- -- -- 
 
 const add = function (int1, int2) {
     return parseInt(int1) + parseInt(int2);
@@ -37,17 +42,28 @@ const divide = function (int1, int2) {
 };
 
 
+// -- -- -- -- -- -- -- - Function operate-- -- -- -- -- -- -- -- -- 
 
 const operate = function (operator, int1, int2) {
     if (operator === '+') return add(int1, int2)
     if (operator === '-') return subtract(int1, int2)
     if (operator === '*') return multiply(int1, int2)
     if (operator === '/') return divide(int1, int2)
-
 };
 
 
+// -- -- -- -- -- -- -- - Function clear-- -- -- -- -- -- -- -- -- 
 
+const clearResult = function () {
+    result.innerText = 0;
+};
+
+
+// -- -- -- -- -- -- -- - Function append value-- -- -- -- -- -- -- -- -- 
+
+const appendValue = function (buttonValue) {
+    result.innerText += buttonValue;
+};
 
 // Event listener -> avoid dragging the display result
 result.addEventListener('dragstart', (e) => e.preventDefault());
