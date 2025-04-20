5737f79774b16b4b71f0505bd9244c52a92f5bf8
Romain <70705788+RomFic@users.noreply.github.com>
2025-04-20 11:26:27 -0400

    worked on operators functions and appendValue

--- script.js
+++ script.js
@@ -11,64 +11,84 @@ const numbers = '1234567890.'
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        const buttonValue = e.target.dataset.value;
+        const value = e.target.dataset.value;
 
-        if (buttonValue === 'clear') {
-            clearResult();
-        } else if (buttonValue === '=') {
-            operate();
+
+
+        if (value === '=') {
+            if (result.innerText.includes('+')) {
+                const splitResult = result.innerText.split('+');
+                appendValue(operate('+', splitResult[0], splitResult[1]));
+            }
+            if (result.innerText.includes('-')) {
+                const splitResult = result.innerText.split('-');
+                appendValue(operate('-', splitResult[0], splitResult[1]));
+            }
+            if (result.innerText.includes('*')) {
+                const splitResult = result.innerText.split('*');
+                appendValue(operate('*', splitResult[0], splitResult[1]));
+            }
+            if (result.innerText.includes('/')) {
+                const splitResult = result.innerText.split('/');
+                appendValue(operate('/', splitResult[0], splitResult[1]));
+            }
+
         } else {
-            appendValue(buttonValue);
+            appendValue(value);
         }
+
+        if (value === 'clear') clearResult();
+
     })
 })
 
 // -- -- -- -- -- -- -- - Functions math operators-- -- -- -- -- -- 
 
 const add = function (int1, int2) {
-    return parseInt(int1) + parseInt(int2);
+    return `${' = '}${(int1 + int2)}`;
 };
 
 const subtract = function (int1, int2) {
-    return parseInt(int1) - parseInt(int2);
+    return `${' = '}${(int1 - int2)}`;
 };
 
 const multiply = function (int1, int2) {
-    return parseInt(int1) * parseInt(int2);
+    return `${' = '}${(int1 * int2)}`;
 };
 
 const divide = function (int1, int2) {
-    return parseInt(int1) / parseInt(int2);
+    if (int2 === 0) {
+        return "Cannot divide by 0";
+    };
+
+    return `${' = '}${(int1 / int2).toFixed(2)}`;
 };
 
 
-// -- -- -- -- -- -- -- - Function operate-- -- -- -- -- -- -- -- -- 
+// -- -- -- -- -- -- -- - Functions -- -- -- -- -- -- -- -- -- 
 
 const operate = function (operator, int1, int2) {
-    if (operator === '+') return add(int1, int2)
-    if (operator === '-') return subtract(int1, int2)
-    if (operator === '*') return multiply(int1, int2)
-    if (operator === '/') return divide(int1, int2)
+    int1 = parseFloat(int1);
+    int2 = parseFloat(int2);
+    if (operator === '+') return add(int1, int2);
+    if (operator === '-') return subtract(int1, int2);
+    if (operator === '*') return multiply(int1, int2);
+    if (operator === '/') return divide(int1, int2);
 };
 
-
-// -- -- -- -- -- -- -- - Function clear-- -- -- -- -- -- -- -- -- 
-
-const clearResult = function () {
-    result.innerText = 0;
-};
-
-
-// -- -- -- -- -- -- -- - Function append value-- -- -- -- -- -- -- -- -- 
 
 const appendValue = function (buttonValue) {
     result.innerText += buttonValue;
 };
 
+const clearResult = function () {
+    result.innerText = '';
+};
+
 // Event listener -> load 0 on display when first loading
-document.addEventListener('DOMContentLoaded', () => {
-    result.innerText = 0;
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     result.innerText = "welcome";
+// });
 
 // Event listener -> avoid dragging the display result
 result.addEventListener('dragstart', (e) => e.preventDefault());
